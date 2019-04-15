7178c2e98048ab537937d49340066f790ee609e6
VincentViers <vincent.viers@sciencespo.fr>
2019-04-15 16:30:57 +0100

    semantic api

--- nytscraper.py
+++ nytscraper.py
@@ -8,23 +8,28 @@ with open(".credentials", "r") as f:
 # create a set to keep track of already seen articles
 seen = set()
 
-# Limit to articles published in the last 24 hours, query limit is 20 anyways...
-url = f'https://api.nytimes.com/svc/news/v3/content/all/all/24.json?api-key={key}'
-
-r = requests.get(url)
-
-while r.status_code != 200:
-    print(f"Something wrong happened... Error code: {r.status_code}. Retrying...")
-    sleep(10)
+while True:
     
-data = r.json()
+    # Limit to articles published in the last 24 hours, query limit is 20 anyways...
+    url = f'https://api.nytimes.com/svc/news/v3/content/all/all/24.json?api-key={key}'
 
-for article in data["results"]:
-    if article['title'] not in seen:
-        print("Found a new article!")
-        
-        print(f"It is called {article['title']}")
-        
-        seen.add(article['title'])
-        
+    r = requests.get(url)
 
+    while r.status_code != 200:
+        print(f"Something wrong happened... Error code: {r.status_code}. Retrying...")
+        sleep(10)
+
+    data = r.json()
+
+    for article in data["results"]:
+        if article['title'] not in seen:
+            print("Found a new article!")
+
+            print(f"It is called {article['title']}")
+
+            seen.add(article['title'])
+                  
+            with open("some_data.txt", "a") as outfile:
+                outfile.write(str(article) + "\n")
+                  
+    sleep(120)
